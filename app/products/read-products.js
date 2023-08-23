18d34ecc04c9284d51cdd13cb2a2eaa7f339c0fe
Rinat Sarmuldin <ura07srr@gmail.com>
2023-08-24 00:15:59 +0500

    Add func showProduct()

--- app/products/read-products.js
+++ app/products/read-products.js
@@ -3,8 +3,12 @@ jQuery(($) => {
     // Показать список товаров при первой загрузке
     showProducts();
 });
-
 // При нажатии кнопки
 $(document).on("click", ".read-products-button", () => {
     showProducts();
 });
+
+// Функция для показа списка товаров
+function showProducts() {
+
+}
