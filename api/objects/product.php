72327c51176e9181f8105f69d3a768503a46895c
Rinat Sarmuldin <ura07srr@gmail.com>
2023-08-23 22:08:35 +0500

    Create method readOne()

--- api/objects/product.php
+++ api/objects/product.php
@@ -75,4 +75,40 @@ class Product
         }
         return false;
     }
+
+    // метод для получения конкретного товара по ID
+    function readOne()
+    {
+        // запрос для чтения одной записи (товара)
+        $query = "SELECT
+            c.name as category_name, p.id, p.name, p.description, p.price, p.category_id, p.created
+        FROM
+            " . $this->table_name . " p
+            LEFT JOIN
+                categories c
+                    ON p.category_id = c.id
+        WHERE
+            p.id = ?
+        LIMIT
+            0,1";
+
+        // подготовка запроса
+        $stmt = $this->conn->prepare($query);
+
+        // привязываем id товара, который будет получен
+        $stmt->bindParam(1, $this->id);
+
+        // выполняем запрос
+        $stmt->execute();
+
+        // получаем извлеченную строку
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        // установим значения свойств объекта
+        $this->name = $row["name"];
+        $this->price = $row["price"];
+        $this->description = $row["description"];
+        $this->category_id = $row["category_id"];
+        $this->category_name = $row["category_name"];
+    }
 }
