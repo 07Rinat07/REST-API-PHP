52c908a8d0c74a14a847d19584c4bac9605d3a2c
Rinat Sarmuldin <ura07srr@gmail.com>
2023-08-24 01:46:03 +0500

    Create HTML form __Create product__

--- app/products/create-product.js
+++ app/products/create-product.js
@@ -19,3 +19,46 @@ jQuery(($) => {
 
     // Здесь будет обработчик «Создать форму товара»
 });
+let create_product_html=`
+
+    <!-- Кнопка для показа всех товаров -->
+    <div id="read-products" class="btn btn-primary pull-right m-b-15px read-products-button">
+        <span class="glyphicon glyphicon-list"></span> Все товары
+    </div>
+    <!-- html форма «Создание товара» -->
+<form id="create-product-form" action="#" method="post" border="0">
+    <table class="table table-hover table-responsive table-bordered">
+
+        <tr>
+            <td>Название</td>
+            <td><input type="text" name="name" class="form-control" required /></td>
+        </tr>
+
+        <tr>
+            <td>Цена</td>
+            <td><input type="number" min="1" name="price" class="form-control" required /></td>
+        </tr>
+
+        <tr>
+            <td>Описание</td>
+            <td><textarea name="description" class="form-control" required></textarea></td>
+        </tr>
+
+        <!-- Список выбора категории -->
+        <tr>
+            <td>Категория</td>
+            <td>` + categories_options_html + `</td>
+        </tr>
+
+        <!-- Кнопка отправки формы -->
+        <tr>
+            <td></td>
+            <td>
+                <button type="submit" class="btn btn-primary">
+                    <span class="glyphicon glyphicon-plus"></span> Создать товар
+                </button>
+            </td>
+        </tr>
+
+    </table>
+</form>`;
