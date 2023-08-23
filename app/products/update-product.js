1c5233865c8beb216aba1662cdd3a673b8baa32b
Rinat Sarmuldin <ura07srr@gmail.com>
2023-08-24 02:05:10 +0500

    Add button __product all__and show form for update product

--- app/products/update-product.js
+++ app/products/update-product.js
@@ -34,7 +34,58 @@ jQuery($ => {
 
                 categories_options_html += `</select>`;
 
-                // Здесь будет HTML для обновления товара
+                // Сохраним html в переменной «update product»
+                let update_product_html = `
+    <div id="read-products" class="btn btn-primary pull-right m-b-15px read-products-button">
+        <span class="glyphicon glyphicon-list"></span> Все товары
+    </div>
+
+    <!-- Построение формы для изменения товара -->
+    <!-- Мы используем свойство "required" html5 для предотвращения пустых полей -->
+    <form id="update-product-form" action="#" method="post" border="0">
+        <table class="table table-hover table-responsive table-bordered">
+
+            <tr>
+                <td>Название</td>
+                <td><input value=\"` + name + `\" type="text" name="name" class="form-control" required /></td>
+            </tr>
+
+            <tr>
+                <td>Цена</td>
+                <td><input value=\"` + price + `\" type="number" min="1" name="price" class="form-control" required /></td>
+            </tr>
+
+            <tr>
+                <td>Описание</td>
+                <td><textarea name="description" class="form-control" required>` + description + `</textarea></td>
+            </tr>
+
+            <tr>
+                <td>Категория</td>
+                <td>` + categories_options_html + `</td>
+            </tr>
+
+            <tr>
+                <!-- Скрытый «идентификатор товара, чтобы определить, какую запись удалить -->
+                <td><input value=\"` + id + `\" name="id" type="hidden" /></td>
+
+                <!-- Кнопка отправки формы -->
+                <td>
+                    <button type="submit" class="btn btn-info">
+                        <span class="glyphicon glyphicon-edit"></span> Обновить товар
+                    </button>
+                </td>
+            </tr>
+
+        </table>
+    </form>
+`;
+
+// Добавим в «page-content» нашего приложения
+                $("#page-content").html(update_product_html);
+
+// Изменим title страницы
+                changePageTitle("Обновление товара");
             });
         });
     });
