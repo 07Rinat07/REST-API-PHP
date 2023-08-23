fb37ebd11d6328274e1761588c6be5e9ce06934a
Rinat Sarmuldin <ura07srr@gmail.com>
2023-08-24 00:24:46 +0500

    Create HTML table

--- app/products/read-products.js
+++ app/products/read-products.js
@@ -21,4 +21,18 @@ function showProducts() {
     <div id="create-product" class="btn btn-primary pull-right m-b-15px create-product-button">
         <span class="glyphicon glyphicon-plus"></span> Создание товара
     </div>
+    <!-- Таблица товаров -->
+<table class="table table-bordered table-hover">
+
+    <!-- Создание заголовков таблицы -->
+    <tr>
+        <th class="w-15-pct">Название</th>
+        <th class="w-10-pct">Цена</th>
+        <th class="w-15-pct">Категория</th>
+        <th class="w-25-pct text-align-center">Действие</th>
+    </tr>\`;
+
+    // Здесь будут строки
+
+read_products_html += \`</table>\`;
 }
