2afc572dc182d0af04d613368ee45b93d1f86493
Rinat Sarmuldin <ura07srr@gmail.com>
2023-08-24 02:02:20 +0500

    Getting info about product

--- app/products/update-product.js
+++ app/products/update-product.js
@@ -5,6 +5,18 @@ jQuery($ => {
 
         // Получаем ID товара
         const id = $(this).attr("data-id");
+        // Читаем одну запись на основе данного идентификатора товара
+        $.getJSON("http://rest-api/api/product/read_one.php?id=" + id, data => {
+
+            // Значения будут использоваться для заполнения нашей формы
+            const name = data.name;
+            const price = data.price;
+            const description = data.description;
+            const category_id = data.category_id;
+            const category_name = data.category_name;
+
+            // Здесь будет загрузка списка категорий
+        });
     });
 
     // Здесь будет обработчик для обновления товара
