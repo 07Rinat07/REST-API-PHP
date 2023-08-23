e83f7de50fff672b4612cbea8653713100cefab1
Rinat Sarmuldin <ura07srr@gmail.com>
2023-08-24 01:43:38 +0500

    Create button category select

--- app/products/create-product.js
+++ app/products/create-product.js
@@ -2,6 +2,14 @@ jQuery(($) => {
 
     // Показать html форму при нажатии кнопки «создать товар»
     $(document).on("click", ".create-product-button", () => {
+        // Перебор возвращаемого списка данных и создание списка выбора
+        let categories_options_html=`<select name="category_id" class="form-control">`;
+
+        $.each(data.records, (key, val) => {
+            categories_options_html+=`<option value="` + val.id + `">` + val.name + `</option>`;
+        });
+
+        categories_options_html += `</select>`;
 
         // Загрузка списка категорий
         $.getJSON("http://rest-api/api/category/read.php", (data) => {
