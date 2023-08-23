addd83719f8610b7f606c4ed562f84c8593a089e
Rinat Sarmuldin <ura07srr@gmail.com>
2023-08-23 22:00:04 +0500

    Notifying the user that the item was not found

--- api/product/read.php
+++ api/product/read.php
@@ -48,4 +48,10 @@ if ($num > 0) {
     echo json_encode($products_arr);
 }
 
-// "товары не найдены" будет здесь
+else {
+    // установим код ответа - 404 Не найдено
+    http_response_code(404);
+
+    // сообщаем пользователю, что товары не найдены
+    echo json_encode(array("message" => "Товары не найдены."), JSON_UNESCAPED_UNICODE);
+}
