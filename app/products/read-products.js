41afed1b7f2c217252502e4253f0f8088ee950d4
Rinat Sarmuldin <ura07srr@gmail.com>
2023-08-24 00:26:45 +0500

    Structura table string for every reg

--- app/products/read-products.js
+++ app/products/read-products.js
@@ -32,7 +32,33 @@ function showProducts() {
         <th class="w-25-pct text-align-center">Действие</th>
     </tr>\`;
 
-    // Здесь будут строки
+   // Перебор списка возвращаемых данных
+$.each(data.records, function (key, val) {
+
+    // Создание новой строки таблицы для каждой записи
+    read_products_html += \`
+        <tr>
+            <td>\` + val.name + \`</td>
+            <td>\` + val.price + \`</td>
+            <td>\` + val.category_name + \`</td>
+
+            <!-- Кнопки "действий" -->
+            <td>
+                <!-- Кнопка чтения товара -->
+                <button class="btn btn-primary m-r-10px read-one-product-button" data-id="\` + val.id + \`">
+                    <span class="glyphicon glyphicon-eye-open"></span> Просмотр
+                </button>
+                <!-- Кнопка редактирования -->
+                <button class="btn btn-info m-r-10px update-product-button" data-id="\` + val.id + \`">
+                    <span class="glyphicon glyphicon-edit"></span> Редактирование
+                </button>
+                <!-- Кнопка удаления товара -->
+                <button class="btn btn-danger delete-product-button" data-id="\` + val.id + \`">
+                    <span class="glyphicon glyphicon-remove"></span> Удаление
+                </button>
+            </td>
+        </tr>\`;
+});
 
 read_products_html += \`</table>\`;
 }
